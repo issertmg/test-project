1889d002fd1524de62bdd6bebbf290eba49d10e4
issertmg <52034895+issertmg@users.noreply.github.com>
2020-07-27 16:53:10 +0800

    removed comment

--- wilsonads/urls.py
+++ wilsonads/urls.py
@@ -27,10 +27,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#     # Serve static and media files from development server
-#     urlpatterns += staticfiles_urlpatterns()
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
